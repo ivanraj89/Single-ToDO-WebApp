d7cb61f93c28dc0e562df815de8ef63acab7a250
ivanraj89 <ivanraj89@gmail.com>
2023-07-30 18:14:10 +0800

    Connecting to Entity Framework

---
+++ SingleToDoWebApp/Models/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SingleToDoWebApp.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) 
+        { 
+
+        }
+
+        public DbSet<ToDoStatement> ToDoStatements { get; set; }
+    }
+}
--- SingleToDoWebApp/Program.cs
+++ SingleToDoWebApp/Program.cs
@@ -1,3 +1,7 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using SingleToDoWebApp.Models;
+
 namespace SingleToDoWebApp
 {
     public class Program
@@ -5,6 +9,11 @@ namespace SingleToDoWebApp
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddDbContext<AppDbContext>(options =>
+            {
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+            });
 
             // Add services to the container.
             builder.Services.AddControllersWithViews();
