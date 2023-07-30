e7c1f9965e564691b0792ac8508ca45cca26c10e
ivanraj89 <ivanraj89@gmail.com>
2023-07-30 16:24:55 +0800

    Commit 1

---
+++ SingleToDoWebApp/Models/ToDoStatement.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SingleToDoWebApp.Models
+{
+    public class ToDoStatement
+    {
+        [Key]
+        public int TaskId { get; set; }
+
+        public string ToDo { get; set; }
+
+        public DateTime date { get; set; }
+    }
+}
